e139636a1de2d1dd26c9f1c2dba2980f45fd5ba9
yasir-sulaiman-aliyum <yasir12sulaiman@gmail.com>
2026-01-11 08:41:15 +0100

    score_calculation

---
+++ .github/calculate_score.py
@@ -0,0 +1,6 @@
+def calculate_score(best_score, bonus=0):
+    total = best_score + bonus
+    return total
+
+    score = int("enter best_score:" + " ")
+    extra = input("enter bonus:" + " ")
---
+++ Score_calculation.py
@@ -0,0 +1,16 @@
+def calculate_score(best_score, bonus=0):
+    total = best_score + bonus
+    return total
+
+
+try:
+    # 30(intiger)
+    score = int(input("enter best_score:"))
+    extra = input("enter bonus:")
+    if extra == "":
+        final = calculate_score(score)
+    else:
+        final = calculate_score(score, int(extra))
+    print(f"result: {final}")
+except ValueError:
+    print("try again")
